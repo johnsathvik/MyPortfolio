3a5b4a962536b4128453eda62568a732686aa193
johnsathvik <johnsathvik@gmail.com>
2026-01-08 22:33:27 -0600

    AWS Systems Manager

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,10 @@ git pull origin main
 source .venv/bin/activate
 pip install -r requirements.txt
 
+#git reset --hard
+#git clean -fd
+#git pull origin main
+
+
 sudo systemctl restart portfolio-main
 sudo systemctl restart portfolio-admin
